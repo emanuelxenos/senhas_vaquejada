1a36c08def032a5483db4ef1058cfaf0bfbb8d6d
emanuelxenos <manuenbs@gmail.com>
2026-05-26 13:52:26 -0300

    fix: altera parametro guest para guests no redirectTo do middleware do laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectTo(
-            guest: function ($request) {
+            guests: function ($request) {
                 if ($request->is('portal/*') || $request->is('portal')) {
                     return route('portal.login');
                 }
